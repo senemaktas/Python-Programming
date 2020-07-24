615dc575bf39834e5262e9af0659d06e6a5fb187
senemaktas <senemaktas9@hotmail.com>
2020-07-24 21:51:39 +0300

    guncelle

---
+++ createPassword.py
@@ -0,0 +1,3 @@
+''' The program creates a random password according to the answer by asking the following questions; 
+-In the password, it is asked whether there are numbers, letters and special characters.
+-The minimum and maximum number of characters should be asked. Special characters -> "'! # @. Will be $.  ''' 
