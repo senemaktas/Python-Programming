9d330a64f9ea73374694c019987d4f16c46e2399
senemaktas <senemaktas9@hotmail.com>
2020-07-26 19:12:47 +0300

    update

--- createPassword.py
+++ createPassword.py
@@ -3,19 +3,31 @@
 -The minimum and maximum number of characters should be asked. Special characters -> '! # @ , . $ ''' 
 
 import random
+import string
 
 option1=input("Do you want to include numbers? [Y/N]:")
-num=random.randint(0,9)
+numberr=list(string.digits)
+num=random.choice(numberr)
 
 option2=input("Do you want to include letters? [Y/N]:")
-letter=["A"]
+letter=list(string.ascii_letters)
 let=random.choice(letter)
 
 option3=input("Do you want to include special characters? [Y/N]:")
-specialcharacter=[" ' " , " ! " , " # " , " @ " , "," , " . " , " $ "]
+specialcharacter=list(string.punctuation)
 specialchar=random.choice(specialcharacter)
 
 option4=input("Please enter minimum number of characters:")
 option5=input("Please enter maximum number of characters:")
 lenPsswd=len(random.randint(min(int(option4)),max(int(option5))))
-print
+print("parola uzunluğu: ",lenPsswd)
+
+passwordNew=[]
+
+def createPass():
+    if option1==Y:
+        passwordNew=passwordNew.append(num)
+    elif option2==Y:
+        passwordNew=passwordNew.append(let)
+    elif option3==Y
+       
