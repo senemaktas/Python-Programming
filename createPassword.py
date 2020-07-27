fb39d092c29c4d0a9889cad10ab9a540ba9e919a
senemaktas <senemaktas9@hotmail.com>
2020-07-27 19:48:03 +0300

    new update

--- createPassword.py
+++ createPassword.py
@@ -6,28 +6,50 @@ import random
 import string
 
 option1=input("Do you want to include numbers? [Y/N]:")
-numberr=list(string.digits)
-num=random.choice(numberr)
+num=list(string.digits)
 
 option2=input("Do you want to include letters? [Y/N]:")
-letter=list(string.ascii_letters)
-let=random.choice(letter)
+lett=list(string.ascii_letters)
 
 option3=input("Do you want to include special characters? [Y/N]:")
-specialcharacter=list(string.punctuation)
-specialchar=random.choice(specialcharacter)
+specialchar=list(string.punctuation)
 
 option4=input("Please enter minimum number of characters:")
 option5=input("Please enter maximum number of characters:")
-lenPsswd=len(random.randint(min(int(option4)),max(int(option5))))
-print("parola uzunluğu: ",lenPsswd)
 
-passwordNew=[]
+#Random password length is determined from the given values.
+passLen=random.randint(int(option4),int(option5))
+print("parola uzunluğu: ",passLen)
 
-def createPass():
-    if option1==Y:
-        passwordNew=passwordNew.append(num)
-    elif option2==Y:
-        passwordNew=passwordNew.append(let)
-    elif option3==Y
-       
+def createPass(passLen):
+    
+    #According to the answers given, it is an empty list that will contain the elements that may be the password.
+    PasswordElements=[]
+
+    if (option1=='Y'):
+        continue
+        PasswordElements += num
+        
+    elif (option2=='Y'):
+        continue
+        PasswordElements += lett
+        
+    elif (option3=='Y'):
+        continue
+        PasswordElements += specialchar
+        continue
+    else:
+        print("list is empty therefore password won't create.")
+    
+    print(PasswordElements)
+
+    #this empty list is gong to contain new password.
+    passwordNew=[]
+
+    #create password 
+    random.shuffle(passElement)
+    passwordNew=passwordNew.append(random.choices(passElement,k=passLen))
+    print(passwordNew)
+return passwordNew
+
+print("Your new random password: ",createPass())
