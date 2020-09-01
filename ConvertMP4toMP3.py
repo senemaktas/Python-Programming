9a8bd3f910e5e12d8be574ad094e190cd22ebdfd
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-01 16:03:47 +0300

    Add files via upload

---
+++ ConvertMP4toMP3.py
@@ -0,0 +1,9 @@
+# Extract mp3 files from an mp4 video
+# 1- install : pip/pip3 install pydub  2- install : sudo apt install ffmpeg
+
+''' AudioSegment is the container to load, transform, and save audio files.
+Python’s pydub module lets you to audio processing tasks that range from video 
+conversion to straight out mixing different files. '''
+from pydub import AudioSegment
+
+AudioSegment.from_file("/home/sidney/Desktop/CopculerKrali_KeyfiBeydeYok.mp4").export("/home/sidney/Desktop//output.mp3", format="mp3")
