1234d62df43e98e83da062c8a47fd1d9a5a46b28
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-21 20:11:12 +0300

    Update and rename SocketServer.py to Socket.py

--- Socket.py
+++ Socket.py
@@ -1,3 +1,4 @@
+# server side -------------------------------------------------
 
 import socket
 import sys
@@ -39,3 +40,35 @@ while 1:
          baglanti.send("Cevap yanlis! "+kullaniciAdi+ " ,tekrar deneyebilirsin.")
 
      sunucuSoketi.close()
+     
+# client side --------------------------------------------------------------------------------     
+     
+import sys
+import socket
+
+host='127.0.0.1'  
+port=12000 
+
+istemciSoketi=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+#istemci sunucuya baglanir.
+istemciSoketi.connect((host,port))
+
+while 1:
+     ilkMesaj=istemciSoketi.recv(1024)
+     print "\nSunucudan gelen hosgeldiniz mesaji: ",ilkMesaj
+      
+     #istemci uygulamayi yazan kisinin ismini gonderir.
+     isim=raw_input()
+     istemciSoketi.sendall(isim)
+
+     GelenSoru=istemciSoketi.recv(1024)
+     print "Sunucudan gelen soru: ",GelenSoru
+
+     #kullanici gelen soruyu cevaplar.
+     sorununCevabi=raw_input("")
+     istemciSoketi.sendall(sorununCevabi)
+
+     sonuc=istemciSoketi.recv(1024)
+     print "Sunucudan gelen sonuc mesaji: ",sonuc
+
+     istemciSoketi.close()
