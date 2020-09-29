efd35a4cf207dd96c143fe9756c5502b79879e0b
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-29 21:09:18 +0300

    Delete ConversionofVideotoGif.py

--- ConversionofVideotoGif.py
+++
@@ -1,6 +0,0 @@
-# https://www.codespeedy.com/conversion-of-video-to-gif-using-python/
-
-from moviepy.editor import *
-
-clip = (VideoFileClip("/home/..../video.mp4"))
-clip.write_gif("/home/..../video.gif")
