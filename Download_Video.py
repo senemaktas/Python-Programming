6de6b7b77f765d8201de1c16190e9ed0aaec889d
senemaktas <30393152+senemaktas@users.noreply.github.com>
2021-07-27 13:08:10 +0300

    Update Download_Video.py

--- Download_Video.py
+++ Download_Video.py
@@ -1,5 +1,11 @@
 !pip install -q youtube-dl
 
+""" Run youtube-dl -F URL to get a list of available formats for video/audio, then choose the one you want and get it using 
+youtube-dl -f [format-number] URL like: youtube-dl -f 43 URL """
+
+# get format number from list 
+!youtube-dl -F https://www.youtube.com/watch?v=jKLrV2pfpoU&list=PL7WS8odYod9ztsoA0xy7pebmfHs23cq_N
+
 # importing module
 import youtube_dl
   
